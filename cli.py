7af3eda48e95ad2614eb4f838ea9f4a5c259d782
Evan Higgins <evan.higgins06@gmail.com>
2022-06-24 23:55:38 -0500

    Added newlines between CLI values.

--- cli.py
+++ cli.py
@@ -12,12 +12,21 @@ def main():
     # By default pandas.DataFrame.__repr__ only prints out the first and last
     # few columns if the max is above a threshold. We want to alwasy see the
     # results, though.
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', None):
-        for v in respList:
+    with pd.option_context(
+        'display.max_rows', None,
+        'display.max_columns', None, 
+        'display.width', None,
+    ):
+        for idx, v in enumerate(respList):
             if isinstance(v, pd.DataFrame):
                 print(v.to_string(index=False))
             else:
                 print(v)
+
+            # Print newlines between values if there are multiple.
+            if idx + 1 < len(respList):
+                print('')
+
 
 if __name__ == "__main__":
     main()
